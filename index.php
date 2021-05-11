36360d2ed1fe47ce1c03a3f45cbf67c82a3a7808
n0id0nt <h4bert44@gmail.com>
2021-05-11 22:51:29 +1000

    added the rest of the web pages

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Bluewave Technology</title>
+    <title>Peoples Health Pharmarcy</title>
 	<meta name="description" content="Peoples Health Pharmarcy Website" />
 	<meta name="keywords"    content="Peoples Health Pharmarcy" />
 	<meta name="author"      content="Glory of the Blue Beard" />
@@ -12,17 +12,14 @@
 <body>
 	<?php include("header.inc"); ?>
 	<div id="main">
-		<?php include("menu.inc"); ?>
-		<article>
-			<h2>About us</h2>
-			<figure>
-				<img  src="images/logo.png" alt="Bluewave Technology Logo"/>
-				<figcaption>Bluewave Technology&reg;</figcaption>
-			</figure>
-			<p>We are Bluewave Technology&reg; and are proud to be labelled Melbourne's 3<sup>rd</sup> fastest growing tech company.</p>
-			<p>We specialize in creating software solutions for small business in Melbourne and have pride in the releantionship we have formed with these business since our conception.</p>
-			<p>Bluewave Technology&reg; is always excited to welcome on board new staff and excited to introduce you to our teams of enthusiastic developers.</p>
-		</article>
+		<nav>
+			<h2>Navigation</h2>
+			<ul>
+				<li><a href="index.php">Add sales records</a></li>
+				<li><a href="index.php">Edit sales records</a></li>
+				<li><a href="index.php">Reports</a></li>
+			</ul>
+		</nav>
 		<?php include("footer.inc"); ?>
 	</div>
 </body>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Bluewave Technology</title>
+	<meta name="description" content="Peoples Health Pharmarcy Website" />
+	<meta name="keywords"    content="Peoples Health Pharmarcy" />
+	<meta name="author"      content="Glory of the Blue Beard" />
+	<link href="styles/assignment2.css" rel="stylesheet" />
+</head>
+<body>
+	<?php include("header.inc"); ?>
+	<div id="main">
+		<?php include("menu.inc"); ?>
+		<article>
+			<h2>About us</h2>
+			<figure>
+				<img  src="images/logo.png" alt="Bluewave Technology Logo"/>
+				<figcaption>Bluewave Technology&reg;</figcaption>
+			</figure>
+			<p>We are Bluewave Technology&reg; and are proud to be labelled Melbourne's 3<sup>rd</sup> fastest growing tech company.</p>
+			<p>We specialize in creating software solutions for small business in Melbourne and have pride in the releantionship we have formed with these business since our conception.</p>
+			<p>Bluewave Technology&reg; is always excited to welcome on board new staff and excited to introduce you to our teams of enthusiastic developers.</p>
+		</article>
+		<?php include("footer.inc"); ?>
+	</div>
+</body>
+</html>
