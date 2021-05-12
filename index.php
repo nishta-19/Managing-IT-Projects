7383be723e3c5ca06ff9f1e049efbe4bf8018f3b
n0id0nt <h4bert44@gmail.com>
2021-05-12 10:50:33 +1000

    added login

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Peoples Health Pharmarcy</title>
 	<meta name="description" content="Peoples Health Pharmarcy Website" />
 	<meta name="keywords"    content="Peoples Health Pharmarcy" />
-	<meta name="author"      content="Glory of the Blue Beard" />
+	<meta name="author"      content="Glory to the BlueBeard" />
 	<link href="styles/assignment2.css" rel="stylesheet" />
 </head>
 <body>
