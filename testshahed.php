a702318d4ed3684db14de916ae9dfe24cfcd73e9
khshahed30 <shahedhasan600@gmail.com>
2021-05-19 18:23:53 +0800

    test page

---
+++ testshahed.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Data Visualisation" />
+    <meta name="keywords" content="HTML, CSS, D3" />
+    <meta name="author" content="Shahed Hasam" />
+
+    <title>Task 2.2 D3 Data Binding</title>
+
+    <script src="http://d3js.org/d3.v5.min.js"></script>
+    <script src="2.2.js"></script>
+</head>
+
+<body>
+    <h1>The D3 Journey starts here...</h1>
+    <p id=chart></p>
+    <br>
+    <footer style="color:grey">COS30045 Data Visualisation<br>
+        Shahed Hasan Khandakar</footer>
+
+</body>
+
+</html>
