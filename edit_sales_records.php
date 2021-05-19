b08c4259f14a9463a87c8740fe2d81112009bb3d
Chris <102569367@student.swin.edu.au>
2021-05-19 10:07:10 +1000

    Edit-sales

--- edit_sales_records.php
+++ edit_sales_records.php
@@ -1,26 +1,47 @@
-<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "phpsprint1";
+include "display_sales_records.php";
 
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <title>Peoples Health Pharmarcy</title>
-	<meta name="description" content="Peoples Health Pharmarcy Website" />
-	<meta name="keywords"    content="Peoples Health Pharmarcy" />
-	<meta name="author"      content="Glory to the BlueBeard" />
-	<link href="styles/page_style.css" rel="stylesheet" />
-</head>
-<body>
-	<?php include("header.inc"); ?>
-	<div id="main">
-		<article>
-			<h2>edit sales records</h2>
-			<form action="welcome_get.php" method="post" id="edit record">
-				Item: <input type="text" name="item"><br>
-				Quantity: <input type="text" name="quantity"><br>
-				<input type="submit" form="edit record" value="Edit Record">
-			</form>
-		</article>
-		<?php include("footer.inc"); ?>
-	</div>
-</body>
-</html>
+
+$connect = new mysqli($servername, $username, $password, $dbname);
+if ($connect->connect_error) {
+	die("Connection failed: " . $connect->connect_error);
+}
+$SalesID = $_GET['id'];
+$resultSales = mysqli_query($connect, "Select * from sales where SalesID = $SalesID");
+$resultProduct = mysqli_query($connect, "Select * from product");
+$row = mysqli_fetch_array($resultSales);
+
+
+
+if(isset($_POST['update'])) 
+{
+    $ProductID = $_POST['ProductID'];
+    $Amount = $_POST['Amount'];
+	$Date = $_POST['Date'];
+	
+    $edit = mysqli_query($connect,"update sales set ProductID='$ProductID', Ammount='$Amount', Date='$Date' where SalesID='$SalesID'");
+	
+        if($edit)
+    {
+        mysqli_close($connect); // Close connection
+        header("location:display_sales_records.php");
+        exit;
+    }	
+}
+?>
+
+<h3>Update Data</h3>
+SalesID: <?php echo $SalesID?>
+<form method="POST">
+  <label for="ProductID">ProductID </label>
+  <input type="text" name="ProductID" value="<?php echo $row['ProductID'] ?>" placeholder="Enter ProductID" Required>
+  <label for="Amount">Amount </label>
+  <input type="text" name="Amount" value="<?php echo $row['Ammount'] ?>" placeholder="Enter Amount" Required>
+  <label for="Date">Sale Date </label>
+  <input type="text" name="Date" value="<?php echo $row['Date'] ?>" placeholder="Enter Date" Required>
+  <input type="submit" name="update" value="Update">
+</form>
