5521dd0d8475eb89193294524abdc928fdce052d
Sxinx <josharceri99@gmail.com>
2021-05-21 15:34:20 +1000

    Change to the sql statement and csv statements to properly display data

--- monthly_sales_db.php
+++ monthly_sales_db.php
@@ -2,39 +2,38 @@
 include 'config.php';
 session_start();
 
-$filename = "Sales_" . $_SESSION['StartDate'] ."_". $_SESSION['EndDate'] . ".csv"; 
-$delimiter = ","; 
- 
+$filename = "Sales_" . $_SESSION['StartDate'] . "_" . $_SESSION['EndDate'] . ".csv";
+$delimiter = ",";
+
 // Create a file pointer 
-$f = fopen('php://memory', 'w'); 
- 
+$f = fopen('php://memory', 'w');
+
 // Set column headers 
-$fields = array('SalesID', 'Date', 'ProductID', 'Ammount', 'UserID', 'Customer'); 
-fputcsv($f, $fields, $delimiter); 
- 
+$fields = array('Sales ID', 'Date', 'Product ID', 'Product Name', 'Product Type', 'Amount', 'Customer');
+fputcsv($f, $fields, $delimiter);
+
 // Get records from the database 
-$stmt = $con->prepare("SELECT * FROM Sales WHERE Date Between ? AND ? ORDER BY SalesID DESC");
+$stmt = $con->prepare("SELECT SalesID, Date, Product.ProductID, Prodname, Prodtype, Ammount, Customer FROM sales JOIN product ON product.ProductID = Sales.ProductID Where Date BETWEEN ? AND ?");
 $stmt->bind_param('ss', $_SESSION['StartDate'], $_SESSION['EndDate']);
 $stmt->execute();
-$result = $stmt->get_result(); 
-if($result->num_rows > 0){ 
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
     // Output each row of the data, format line as csv and write to file pointer 
-    while($row = $result->fetch_assoc()){ 
-        $lineData = array($row['SalesID'], $row['Date'], $row['ProductID'], $row['Ammount'], $row['UserID'], $row['Customer']); 
-        fputcsv($f, $lineData, $delimiter); 
-    } 
-} 
- 
+    while ($row = $result->fetch_assoc()) {
+        $lineData = array($row['SalesID'], $row['Date'], $row['ProductID'], $row['Prodname'], $row['Prodtype'], $row['Ammount'], $row['Customer']);
+        fputcsv($f, $lineData, $delimiter);
+    }
+}
+
 // Move back to beginning of file 
-fseek($f, 0); 
- 
+fseek($f, 0);
+
 // Set headers to download file rather than displayed 
-header('Content-Type: text/csv'); 
-header('Content-Disposition: attachment; filename="' . $filename . '";'); 
- 
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="' . $filename . '";');
+
 // Output all remaining data on a file pointer 
-fpassthru($f); 
- 
+fpassthru($f);
+
 // Exit from file 
 exit();
-?>
