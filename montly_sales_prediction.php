59ebc792f0ccd2ce7bb9891a3900caa8a8933ed5
Your Name <nishtapatel19@gmail.com>
2021-05-26 01:46:47 +1000

    commit

---
+++ montly_sales_prediction.php
@@ -0,0 +1,18 @@
+?php
+include 'config.php';
+include 'head.php';
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+  header('Location: login.php');
+  exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<body>
+  <?php include("header.inc"); ?>
+  <article>
+    <h2>Monthly Sales Prediction</h2>
