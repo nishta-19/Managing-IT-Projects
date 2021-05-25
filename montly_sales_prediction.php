a84b6e04b5a63000aa1a44c996bc09e2fc96ec99
Your Name <nishtapatel19@gmail.com>
2021-05-26 03:42:12 +1000

    commit

--- montly_sales_prediction.php
+++ montly_sales_prediction.php
@@ -12,13 +12,26 @@ if (!isset($_SESSION['loggedin'])) {
 <!DOCTYPE html>
 <html>
 
+<head>
+     <link href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.css" rel="stylesheet">
+     <link href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css" rel="stylesheet">
+</head>
+
 <body>
+
   <?php include("header.inc"); ?>
   <article>
     <h2>Monthly Sales Prediction</h2>
     <p>The chart shows predicted sales for next month.</p>
 
-  
+    <canvas id="mychart" style="height:auto; width: 500px;"></canvas>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+
+    
 
 
   </article>
